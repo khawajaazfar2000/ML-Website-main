6f721f7ba88ae826f70c3a255437e0d162358c30
khawajaazfar2000 <khawajaazfar2000@gmail.com>
2023-02-02 23:13:20 +0500

    Files

---
+++ Self-Driving-Car/controls.js
@@ -0,0 +1,53 @@
+class Controls {
+  constructor(type) {
+    this.left = false;
+    this.right = false;
+    this.forward = false;
+    this.reverse = false;
+
+    switch (type) {
+      case "KEYS":
+        this.#addKeyboardListeners();
+        break;
+      case "DUMMY":
+        this.forward = true;
+        break;
+    }
+  }
+  #addKeyboardListeners() {
+    document.addEventListener("keydown", (e) => {
+      switch (e.key) {
+        case "ArrowLeft":
+          this.left = true;
+          break;
+        case "ArrowRight":
+          this.right = true;
+          break;
+        case "ArrowUp":
+          this.forward = true;
+          break;
+        case "ArrowDown":
+          this.reverse = true;
+          break;
+      }
+      // console.table(this);
+    });
+    document.addEventListener("keyup", (e) => {
+      switch (e.key) {
+        case "ArrowLeft":
+          this.left = false;
+          break;
+        case "ArrowRight":
+          this.right = false;
+          break;
+        case "ArrowUp":
+          this.forward = false;
+          break;
+        case "ArrowDown":
+          this.reverse = false;
+          break;
+      }
+      // console.table(this);
+    });
+  }
+}
